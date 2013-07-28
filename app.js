56b0f7d90e03f7fd9047d7779d586dd42b54b5a3
Noel King <noelking@gmail.com>
2013-07-28 18:56:27 +0100

    Noelworking

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ app.use(express.bodyParser());
 // SEE: https://github.com/bu/Accessor_MySQL for DB instructions
 var Accessor = require("Accessor")
 db = {
-	user: Accessor("users", "MySQL")
+	user: Accessor("users", "MySQL"),
+	activity: Accessor("activity", "MySQL")
+	
 }
 
 var user = require('./lib/user')
+var activity = require('./lib/activity')
 var responses = require('./lib/responses')
 
 app.use(user)
+app.use(activity)
+app.use(responses)
 
 app.listen(process.env.PORT || 3000)
 console.log("Listening on port "+(process.env.PORT || 3000))
---
+++ runDevScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DB_HOST=80.93.28.105
+DB_USER=coolestapp
+DB_PASSWORD=coderdojo
+DB_NAME=coolestprojectsapp
+export DB_HOST DB_USER DB_PASSWORD DB_NAME
+node app.js
