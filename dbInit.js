23922569ff115090366abcb2c80fb300c7665257
Ben Chapman <bchapman@engineyard.com>
2013-07-29 17:07:45 +0100

    Fix that sutff

--- dbInit.js
+++ dbInit.js
@@ -5,11 +5,11 @@ db = new sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
 	port: dbConfig.port
 })
 var userModel = require("./lib/user/model")
-db.sync({force: true}).success(function(){
+userModel.dbTable.sync({force: true}).success(function(){
 	userModel.dbTable.create({
 		name: "Dan Iuffy",
 		email: "dmiuffy15@example.com",
-		password: "5f4dcc3b5aa765d61d8327deb882cf99",
+		password: "098f6bcd4621d373cade4e832627b4f6",
 		dojo: "DCU",
 		session_hash: "7574d936a15fd075d2d82c5e87b64f7c"
 	})
