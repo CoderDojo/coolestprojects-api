c845f7a34f6287e2fe8593db0a1b4d8eccef15c4
Ben Chapman <bchapman@engineyard.com>
2013-07-29 16:45:40 +0100

    Update for new models

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 var databaseConfig = { 
     user: process.env['DB_USER'] || "root",
     password: process.env['DB_PASSWORD'] || "",
-    database: process.env['DB_NAME'] || "coolestprojects",
+    database: process.env['DB_NAME'] || process.env['APP_NAME'] || "coolestprojects",
     host: process.env['DB_HOST'] || "localhost",
     port: 3306
 };
--- dbInit.js
+++ dbInit.js
@@ -6,7 +6,7 @@ db = new sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
 })
 var userModel = require("./lib/user/model")
 db.sync({force: true}).success(function(){
-	userModel.create({
+	userModel.dbTable.create({
 		name: "Dan Iuffy",
 		email: "dmiuffy15@example.com",
 		password: "5f4dcc3b5aa765d61d8327deb882cf99",
