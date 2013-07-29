ca95d407d43ffe6031b7a8f5f3be745467701b20
Ben Chapman <bchapman@engineyard.com>
2013-07-29 15:22:27 +0100

    Boom, sequelize

--- app.js
+++ app.js
@@ -2,20 +2,6 @@ var express = require('express')
 var app = express()
 app.use(express.bodyParser());
 
-var dbConfig = require('./config/database')
-var sequelize = require("sequelize")
-db = new sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
-	host: dbConfig.host,
-	port: dbConfig.port
-})
-var userModel = require("./lib/user/model")
-
-app.get('/', function(req,res) {
-	userModel.find({ where: {id: 1}}).success(function(userRecord){
-		res.json(userRecord)
-	});
-})
-
 var user = require('./lib/user')
 var activity = require('./lib/activity')
 var responses = require('./lib/responses')
